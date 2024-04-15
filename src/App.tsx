020ed95a3a65290447129659a7604126b801d5f9
SanyaSkl <90842468+SanyaSkl@users.noreply.github.com>
2024-04-15 22:54:20 +0300

    ratings refactoring

--- src/App.tsx
+++ src/App.tsx
@@ -7,14 +7,14 @@ function App() {
     console.log("App rendering")
     return (
         <div>
-            <PageTitle title={"This is APP component"}/>
-            <PageTitle title={"My friends"}/>
-            Article 1
-            <Rating value={3}/>
             <Accordion titleValue={"Menu"} collapsed={true}/>
             <Accordion titleValue={"Users"} collapsed={false}/>
-            Article 2
+
+            <Rating value={0}/>
+            <Rating value={2}/>
+            <Rating value={3}/>
             <Rating value={4}/>
+            <Rating value={5}/>
         </div>
     );
 }
