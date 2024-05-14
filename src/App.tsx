f83b438191c9a5ff51dfd446e295538070b91a7b
SanyaSkl <90842468+SanyaSkl@users.noreply.github.com>
2024-05-15 00:54:12 +0300

    learning useState

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,26 @@
 import React from 'react';
 import './App.css';
 import Accordion from "./components/Accordion/Accordion";
-import {Rating} from "./components/Rating";
+import {Rating} from "./components/Rating/Rating";
+import OnOff from "./components/OnOff/OnOff";
 
 function App() {
     console.log("App rendering")
     return (
         <div>
-            <Accordion titleValue={"Menu"} collapsed={true}/>
+
+            <OnOff />
+            {/*<OnOff on={true}/>*/}
+            {/*<OnOff on={false}/>*/}
+
+{/*            <Accordion titleValue={"Menu"} collapsed={true}/>
             <Accordion titleValue={"Users"} collapsed={false}/>
 
             <Rating value={0}/>
             <Rating value={2}/>
             <Rating value={3}/>
             <Rating value={4}/>
-            <Rating value={5}/>
+            <Rating value={5}/>*/}
         </div>
     );
 }
