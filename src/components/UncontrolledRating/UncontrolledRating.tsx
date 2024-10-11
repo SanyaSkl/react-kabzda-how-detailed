10a8e4612ddaa81ac99ae6d820fe57d7550500e9
SanyaSkl <90842468+SanyaSkl@users.noreply.github.com>
2024-10-11 23:56:39 +0300

    Storybook 1.5 - added UncontrolledRating

--- src/components/UncontrolledRating/UncontrolledRating.tsx
+++ src/components/UncontrolledRating/UncontrolledRating.tsx
@@ -1,19 +1,25 @@
 import React, {useState} from "react";
 
-type UncontrolledRatingPropsType = {}
 
-export function UncontrolledRating(props: UncontrolledRatingPropsType) {
+export type RatingValueType = 0 | 1 | 2 | 3 | 4 | 5
+
+type RatingPropsType = {
+    defaultValue?: RatingValueType
+    onChange: (value: RatingValueType) => void
+}
+
+export function UncontrolledRating(props: RatingPropsType) {
     console.log("Rating rendering")
 
-    let [value, setValue] = useState(0)
+    let [value, setValue] = useState<RatingValueType>(props.defaultValue ? props.defaultValue : 0)
 
     return (
         <div>
-            <Star selected={value > 0} setValue={setValue} value={1}/>
-            <Star selected={value > 1} setValue={setValue} value={2}/>
-            <Star selected={value > 2} setValue={setValue} value={3}/>
-            <Star selected={value > 3} setValue={setValue} value={4}/>
-            <Star selected={value > 4} setValue={setValue} value={5}/>
+            <Star selected={value > 0} setValue={ () => {setValue(1); props.onChange(1) }} value={1}/>
+            <Star selected={value > 1} setValue={ () => {setValue(2); props.onChange(2) }} value={2}/>
+            <Star selected={value > 2} setValue={ () => {setValue(3); props.onChange(3) }} value={3}/>
+            <Star selected={value > 3} setValue={ () => {setValue(4); props.onChange(4) }} value={4}/>
+            <Star selected={value > 4} setValue={ () => {setValue(5); props.onChange(5) }} value={5}/>
         </div>
     )
 }
