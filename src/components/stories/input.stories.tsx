268902394cac9b5abbcea9ae2f3ab79b4b953a35
SanyaSkl <90842468+SanyaSkl@users.noreply.github.com>
2024-10-17 23:04:24 +0300

    Storybook 1.8 - Controlled input, FLUX

--- src/components/stories/input.stories.tsx
+++ src/components/stories/input.stories.tsx
@@ -14,7 +14,7 @@ export const TrackValueOfUncontrolledInput = () => {
         setValue(actualValue)
     }
 
-    return <><input onChange={onChange}/>  - {value} </>
+    return <><input onChange={onChange}/> - {value} </>
 }
 
 export const GetValueOfUncontrolledInputByButtonPress = () => {
@@ -26,18 +26,32 @@ export const GetValueOfUncontrolledInputByButtonPress = () => {
         setValue(el.value)
     }
 
-    return <><input ref={inputRef} />
+    return <><input ref={inputRef}/>
         <button onClick={save}>save</button>
         - actual value: {value} </>
 }
 
-export const ControlledInput = () => {
-    const [parentValue, setParentValue] = useState('')
+export const ControlledCheckbox = () => {
+    const [parentValue, setParentValue] = useState(true)
 
     const onChange = (e: ChangeEvent<HTMLInputElement>) => {
+        setParentValue(e.currentTarget.checked)
+    }
+    return <input type='checkbox' checked={parentValue} onChange={onChange}/>
+}
+
+export const ControlledSelect = () => {
+    const [parentValue, setParentValue] = useState<string | undefined>('0')
+
+    const onChange = (e: ChangeEvent<HTMLSelectElement>) => {
         setParentValue(e.currentTarget.value)
     }
-    return <input value={parentValue} onChange={onChange}/>
+    return <select>
+        <option>none</option>
+        <option value={'1'}>Minsk</option>
+        <option value={'2'}>Moscow</option>
+        <option value={'3'}>Kiev</option>
+    </select>
 }
 
 export const ControlledInputWithFixedValue = () => <input value={'it-incubator'}/>
