150f69671d9c798f058be64b772eaa8b55ce6a7f
SanyaSkl <90842468+SanyaSkl@users.noreply.github.com>
2024-11-09 14:21:02 +0300

    Storybook 1.13 - useMemo, Helps To React Memo

--- src/components/stories/UseMemo.stories.tsx
+++ src/components/stories/UseMemo.stories.tsx
@@ -1,4 +1,4 @@
-import {useMemo, useState} from "react";
+import React, {useMemo, useState} from "react";
 
 
 export default {
@@ -48,46 +48,34 @@ export const DifficultCountingExample = () => {
     </>
 }
 
+const UsersSecret = (props: { users: Array<string> }) => {
+    console.log('USERS SECRET')
+    return <div>{
+        props.users.map((u, i) => <div key={i}>{u}</div>)
+    }</div>
+}
 
-export const HelpsFprReactMemoExample = () => {
+const Users = React.memo(UsersSecret)
 
-    const [a, setA] = useState<number>(5)
-    const [b, setB] = useState<number>(5)
+export const HelpsToReactMemo = () => {
+    console.log('HelpsToReactMemo')
+    const [counter, setCounter] = useState(0)
+    const [users, setUsers] = useState(['Alex', 'Olga', 'Mika'])
 
-    let resultA = 1
-    let resultB = 1
+    const newArray = useMemo(() => {
+        const newArray = users.filter(u => u.toLowerCase().indexOf('a') > -1)
+        return newArray
+    }, [users])
 
-    resultA = useMemo(() => {
-        let tempResultA = 1
-        for (let i = 1; i <= a; i++) {
-            let fake = 0
-            while (fake < 100000000) {
-                fake++
-                const fakeValue = Math.random()
-            }
-            resultA = resultA * i
-
-        }
-        return resultA
-    }, [a])
-
-
-    for (let i = 1; i <= b; i++) {
-        resultB = resultB * i
+    const addUser = () => {
+        const newUsers = [...users, 'Alice' + new Date().getTime()]
+        setUsers(newUsers)
     }
 
-
     return <>
-        <input value={a} onChange={(e) => setA(Number(e.currentTarget.value))}/>
-        <input value={b} onChange={(e) => setB(+e.currentTarget.value)}/>
-
-        <hr/>
-
-        <div>
-            Result for a: {resultA}
-        </div>
-        <div>
-            Result for b: {resultB}
-        </div>
+        <button onClick={() => setCounter(counter + 1)}>+</button>
+        <button onClick={() => addUser()}>add user</button>
+        {counter}
+        <Users users={newArray}/>
     </>
 }
