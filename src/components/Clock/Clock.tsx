6416b290a6ef1f015dcdbae82b9322ca3a03381a
SanyaSkl <90842468+SanyaSkl@users.noreply.github.com>
2024-12-09 09:59:23 +0300

    Storybook 1.17 - useEffect, component clock

---
+++ src/components/Clock/Clock.tsx
@@ -0,0 +1,36 @@
+import React, {useEffect, useState} from "react";
+
+
+type PropsType = {}
+
+const get2digitsString = (num: number) => num < 10 ? "0" + num : num
+
+export const Clock: React.FC<PropsType> = (props) => {
+    const [date, setDate] = useState(new Date())
+
+    useEffect(() => {
+        const intervalID = setInterval(() => {
+            console.log('TICK')
+            setDate(new Date())
+        }, 1000)
+
+        return () => {
+            clearInterval(intervalID)
+        }
+    }, [])
+
+
+
+    const secondsString = get2digitsString(date.getSeconds())
+    const minutesString = get2digitsString(date.getMinutes())
+    const hoursString = get2digitsString(date.getHours())
+
+
+    return <div>
+        <span>{hoursString}</span>
+        :
+        <span>{minutesString}</span>
+        :
+        <span>{secondsString}</span>
+    </div>
+}
