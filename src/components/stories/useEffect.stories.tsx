65993b88bb070601f87048cd1111848b3be44be7
SanyaSkl <90842468+SanyaSkl@users.noreply.github.com>
2024-12-09 09:59:23 +0300

    Storybook 1.18 - useEffect, component clock

--- src/components/stories/useEffect.stories.tsx
+++ src/components/stories/useEffect.stories.tsx
@@ -1,4 +1,5 @@
 import {useEffect, useState} from "react";
+import {clearInterval} from "timers";
 
 export default {
     title: 'useEffect demo'
@@ -27,8 +28,82 @@ export const SimpleExample = () => {
     }, [counter])
 
     return <>
-    Hello, {counter} {fake}
+        Hello, {counter} {fake}
         <button onClick={() => setFake(fake + 1)}>fake+</button>
         <button onClick={() => setCounter(counter + 1)}>counter+</button>
     </>
 }
+
+export const setTimeoutExample = () => {
+    const [fake, setFake] = useState(1)
+    const [counter, setCounter] = useState(1)
+
+    console.log("setTimeoutExample")
+
+    useEffect(() => {
+
+        setTimeout(() => {
+            console.log("setTimeout")
+            document.title = counter.toString()
+        }, 1000);
+
+    }, [counter])
+
+    return <>
+        Hello, {counter} {fake}
+        <button onClick={() => setFake(fake + 1)}>fake+</button>
+        <button onClick={() => setCounter(counter + 1)}>counter+</button>
+    </>
+}
+
+export const setIntervalExample = () => {
+    const [fake, setFake] = useState(1)
+    const [counter, setCounter] = useState(1)
+
+    console.log("setIntervalExample")
+
+    useEffect(() => {
+
+        setInterval(() => {
+            setCounter((state) => state + 1)
+        }, 3000);
+
+    }, [])
+
+    return <>
+        Hello, counter: {counter} - fake: {fake}
+        {/*<button onClick={() => setFake(fake + 1)}>fake+</button>*/}
+        {/*<button onClick={() => setCounter(counter + 1)}>counter+</button>*/}
+    </>
+}
+
+// export const setNewDateExample = () => {
+//
+//     const [time, setTime] = useState(new Date())
+//
+//     useEffect(() => {
+//         const interval = setInterval(() => {
+//             setTime(new Date())
+//         }, 1000);
+//         return () => clearInterval(interval)
+//     }, [])
+//
+//     const hours = time.getHours();
+//     const minutes = time.getMinutes();
+//     const seconds = time.getSeconds()
+//
+//     const timeString = `${hours}:${minutes}:${seconds}`
+//
+// //     return (
+// //         <div>
+// //             {/* Display the time string */}
+// //             <h1>{timeString}</h1>
+// //         </div>
+// //     );
+// // }
+//
+//
+//     return <>
+//         Hello, {timeString}
+//     </>
+// }
